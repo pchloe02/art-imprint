86fe81b6c9484cfb32ec19e2c880babe40b59bee
pchloe02 <146726419+pchloe02@users.noreply.github.com>
2024-04-17 17:55:53 +0200

    structure header & css

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,22 +5,24 @@ import "https://kit.fontawesome.com/6a21cd18bd.js";
 export default function Header() {
   return (
     <div className='vertical-header'>
-      <h1>Art <br/> Imprint</h1>
+        <div className='logo-wrap'>
+            <h1 className='logo'>Art <br/> Imprint</h1>
+        </div>
 
-      <div className='nav-bar'>
-        <ul className='main-nav'>
-        <li><i className="fa-solid fa-house"></i> Accueil</li>
-        <li><i class="fa-solid fa-user"></i> Profil</li>
-        <li><i class="fa-solid fa-chart-simple"></i> Statistiques</li>
-        <li><i class="fa-solid fa-flag"></i> Objectifs</li>
-        <li><i class="fa-solid fa-plus"></i> Ajouter</li>
-        </ul>
+        <div className='nav-bar'>
+             <ul className='main-nav'>
+                <li className='nav-icon'><i className="fa-solid fa-house"></i>Accueil</li>
+                <li className='nav-icon'><i class="fa-solid fa-user"></i>Profil</li>
+                <li className='nav-icon'><i class="fa-solid fa-chart-simple"></i>Statistiques</li>
+                <li className='nav-icon'><i class="fa-solid fa-flag"></i>Objectifs</li>
+                <li className='nav-icon'><i class="fa-solid fa-plus"></i>Ajouter</li>
+            </ul>
+        </div>
 
-        <ul className='foot-bar'>
-            <li><i class="fa-solid fa-right-from-bracket"></i></li>
-            <li><i class="fa-solid fa-gear"></i></li>
-        </ul>
-      </div>
+            <ul className='foot-bar'>
+                <li><i className="fa-solid fa-right-from-bracket"></i></li>
+                <li><i className="fa-solid fa-gear"></i></li>
+            </ul>
     </div>
   )
 }
